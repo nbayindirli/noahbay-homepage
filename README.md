3c1740d14220ae3af8c8427109c35d16a193b366
Noah Bayindirli 🥂 <noahbay7@gmail.com>
2022-03-23 16:24:46 -0700

    Complete basic homepage with links

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Getting Started with Create React App
+# noahbay-homepage: [noahbay.io](https://www.noahbay.io)
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Homepage for the [noahbay.io](https://www.noahbay.io) website.
+
+---
 
 ## Available Scripts
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <img src={logo} className="App-logo" alt="logo" />
+
+                <h1>
+                    welcome to <a
+                        className="App-link App-link-title"
+                        href=""
+                        //   target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        noahbay<span>.</span>io
+                    </a>
+                </h1>
+
+                <p>
+                    Available subdomains:
+                </p>
+                <a
+                    className="App-link"
+                    href="https://greet.noahbay.io"
+                    //   target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    greet.*
+                </a>
+
+                <p>
+                    Additional websites:
+                </p>
+                <a
+                    className="App-link"
+                    href="https://pawzone.io/"
+                    //   target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    pawzone.io
+                </a>
+                <a
+                    className="App-link"
+                    href="https://samcap.ltd/"
+                    //   target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    samcap.ltd
+                </a>
+
+                <p>
+                    Other links:
+                </p>
+                <a
+                    className="App-link"
+                    href="https://github.com/nbayindirli"
+                    //   target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    github
+                </a>
+                <a
+                    className="App-link"
+                    href="https://www.linkedin.com/in/noahbay/"
+                    //   target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    linkedin
+                </a>
+                <a
+                    className="App-link"
+                    href="https://twitter.com/Noah_Bayindirli"
+                    //   target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    twitter
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
