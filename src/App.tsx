ffabe5552e7bae7e174a484e28bb5c9fecd717d5
Noah Bayindirli 🥂 <noahbay7@gmail.com>
2022-03-23 18:01:25 -0700

    Add FadeIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,136 @@
+import React from 'react';
 import logo from './logo.svg';
+import FadeIn from 'react-fade-in';
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
+        <div className='App'>
+            <header className='App-header'>
 
-                <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} className='App-logo' alt='logo' />
 
-                <h1 id="title">
+                <h1 id='title'>
                     welcome to <span>*</span><a
-                        className="App-link App-link-title"
-                        href=""
-                        //   target="_blank"
-                        rel="noopener noreferrer"
+                        className='App-link App-link-title'
+                        href=''
+                        //   target='_blank'
+                        rel='noopener noreferrer'
                     >
                         .noahbay<span>.</span>io
                     </a>
                 </h1>
-                <p id="disclaimer">...my <span>frontend</span> personality</p>
+                <p id='disclaimer'>...my <span>frontend</span> personality</p>
 
-                <p>
-                    Available subdomains:
-                </p>
-                <a
-                    className="App-link"
-                    href="https://greet.noahbay.io"
-                    //   target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    greet.<span className="link-wildcard">*</span>
-                </a>
+                <FadeIn>
+                    <div>
+                        <p>
+                            Available subdomains:
+                        </p>
+                        <div className='linkHover'>
+                            <a
+                                className='App-link'
+                                href='https://greet.noahbay.io'
+                                //   target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                greet.<span className='link-wildcard'>*</span>
+                            </a>
+                        </div>
+                    </div>
 
-                <p>
-                    Additional websites:
-                </p>
-                <a
-                    className="App-link"
-                    href="https://www.samcap.ltd/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    samcap.ltd
-                </a>
-                <a
-                    className="App-link"
-                    href="https://www.justwalkout.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    justwalkout.com
-                </a>
-                <a
-                    className="App-link"
-                    href="https://developer.amazon.com/alexa/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    developer.amazon.com/alexa
-                </a>
-                <a
-                    className="App-link"
-                    href="https://www.pawzone.io/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    pawzone.io
-                </a>
-                <a
-                    className="App-link"
-                    href="https://www.pawzaar.io/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    pawzaar.io [coming soon]
-                </a>
+                    <div>
+                        <p>
+                            Additional websites:
+                        </p>
+                        <div className='linkHover'>
+                            <a
+                                className='App-link'
+                                href='https://www.samcap.ltd/'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                samcap.ltd
+                            </a>
+                        </div>
+                        <div className='linkHover'>
+                            <a
+                                className='App-link'
+                                href='https://www.justwalkout.com/'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                justwalkout.com
+                            </a>
+                        </div>
+                        <div className='linkHover'>
+                            <a
+                                className='App-link'
+                                href='https://developer.amazon.com/alexa/'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                developer.amazon.com/alexa
+                            </a>
+                        </div>
+                        <div className='linkHover'>
+                            <a
+                                className='App-link'
+                                href='https://www.pawzone.io/'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                pawzone.io
+                            </a>
+                        </div>
+                        <div className='linkHover'>
+                            <a
+                                className='App-link'
+                                href='https://www.pawzaar.io/'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                pawzaar.io [coming soon]
+                            </a>
+                        </div>
+                    </div>
 
-                <p>
-                    Other links:
-                </p>
-                <a
-                    className="App-link"
-                    href="https://github.com/nbayindirli"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    github
-                </a>
-                <a
-                    className="App-link"
-                    href="https://www.linkedin.com/in/noahbay/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    linkedin
-                </a>
-                <a
-                    className="App-link"
-                    href="https://twitter.com/Noah_Bayindirli"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    twitter
-                </a>
+                    <div>
+                        <p>
+                            Other links:
+                        </p>
+                        <div className='linkHover'>
+                            <a
+                                className='App-link'
+                                href='https://github.com/nbayindirli'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                github
+                            </a>
+                        </div>
+                        <div className='linkHover'>
+                            <a
+                                className='App-link'
+                                href='https://www.linkedin.com/in/noahbay/'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                linkedin
+                            </a>
+                        </div>
+                        <div className='linkHover'>
+                            <a
+                                className='App-link'
+                                href='https://twitter.com/Noah_Bayindirli'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                twitter
+                            </a>
+                        </div>
+                    </div>
+                </FadeIn>
             </header>
         </div>
     );
